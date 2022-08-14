8997fd791b73eb7550c2567828855effc78999c5
Olga <o.panova27@gmail.com>
2022-08-14 14:50:12 +0300

    Code review

--- main.py
+++ main.py
@@ -4,41 +4,49 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 import datetime
 import pandas
-from pprint import pprint
 import collections
+import argparse
 
-def get_year():
+
+def get_age():
     today = datetime.datetime.now()
-    year = today.year - 1920
-    if year % 10 == 1 and year != 11 and year % 100 != 11:
-        text_year = "год"
-    elif 1 < year % 10 <= 4 and year != 12 and year != 13 and year != 14:
-        text_year = "года"
+    foundation_year = 1920
+    number_years = today.year - foundation_year
+    if number_years % 10 == 1 and number_years % 100 != 11 and number_years != 11:
+        year_word = "год"
+    elif 1 < number_years % 10 <= 4 and number_years not in [12,13,14]:
+        year_word = "года"
     else:
-        text_year = "лет"
-    return f'{year} {text_year}'
+        year_word = "лет"
+    return f'{number_years} {year_word}'
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
-excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
-wines = excel_data_df.to_dict(orient='records')
-wines_by_category = collections.defaultdict(list)
-for wine in wines:
-    category = wine['Категория']
-    wines_by_category[category].append(wine)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', default='wine.xlsx')
+    parser.add_argument('--sheet', default='Лист1')
+    args = parser.parse_args()
+    excel_data_df = pandas.read_excel(args.file, sheet_name=args.sheet, na_values=['N/A', 'NA'], keep_default_na=False)
 
-rendered_page = template.render(
-    year=get_year(),
-    wines_by_category=wines_by_category
-)
+    wines = excel_data_df.to_dict(orient='records')
+    wines_by_category = collections.defaultdict(list)
+    for wine in wines:
+        category = wine['Категория']
+        wines_by_category[category].append(wine)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        year=get_age(),
+        wines_by_category=wines_by_category
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
